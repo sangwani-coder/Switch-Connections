9cb70325c2dc453cc63c060e08f2b8622c7a66c7
Zyambo <mightypz@gmail.com>
2023-09-09 18:00:54 +0200

    update the admin panel

--- mules_site/SwitchConnections/admin.py
+++ mules_site/SwitchConnections/admin.py
@@ -6,21 +6,42 @@ from .models import CoverImage
 from .models import ContactFormSubmissions, ContactInformation
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['project_name', 'project_description', 'project_category']
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+     list_display = ['category_name', 'category_description']
+
+class CompanyInforAdmin(admin.ModelAdmin):
+    list_display = ['mission', 'vision', 'history']
+
+class TeamMembersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'bio']
+    
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'mobile', 'message', 'created_at']
+    
+class ContactInformationAdmin(admin.ModelAdmin):
+    list_display = ['physical_address', 'phone_number_1', 'phone_number_1', 'email_address']
+    
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'category_description']
+
 class ServiceListingAdmin(admin.ModelAdmin):
-    fields = ['service_name', 'service_description', 'service_price']
+    list_display = ['service_name', 'service_description', 'service_price', 'service_category']
 
 
 #ABOUT_US
-admin.site.register(CompanyInformation)
-admin.site.register(TeamMembers)
+admin.site.register(CompanyInformation, CompanyInforAdmin)
+admin.site.register(TeamMembers, TeamMembersAdmin)
 #CONTACT US
-admin.site.register(ContactFormSubmissions)
-admin.site.register(ContactInformation)
+admin.site.register(ContactFormSubmissions, ContactFormAdmin)
+admin.site.register(ContactInformation, ContactInformationAdmin)
 #HOME
 admin.site.register(CoverImage)
 #PORTFOLIO
-admin.site.register(ProjectCategory)
-admin.site.register(Project)
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
 #SERVICES
 admin.site.register(ServiceListings, ServiceListingAdmin)
-admin.site.register(ServiceCategory)
+admin.site.register(ServiceCategory, ServiceCategoryAdmin)
